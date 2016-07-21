f1030fb385173eeb5c8f9bd6d9cde669790290c5
Casey Jaymes <cjaymes@gmail.com>
2016-07-21 10:57:07 -0400

    to model.Simple from Model

--- scap/model/ai_1_1/SyntheticID.py
+++ scap/model/ai_1_1/SyntheticID.py
@@ -15,9 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
 
-from scap.Model import Model
+from scap.model.Simple import Simple
 import logging
 
 logger = logging.getLogger(__name__)
-class SyntheticID(Model):
+class SyntheticID(Simple):
     pass
