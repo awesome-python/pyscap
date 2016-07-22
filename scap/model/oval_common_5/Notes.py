5100edfb01f273bb0d234ca2d92339e1e159a548
Casey Jaymes <cjaymes@gmail.com>
2016-07-22 11:39:53 -0400

    stubbed oval common

---
+++ scap/model/oval_common_5/Notes.py
@@ -0,0 +1,26 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+from scap.model.Simple import Simple
+import logging
+
+logger = logging.getLogger(__name__)
+class Notes(Simple):
+    def __init__(self):
+        super(Notes, self).__init__()
+        self.tag_name = 'notes'
+        self.ignore_sub_elements.extend(['{http://oval.mitre.org/XMLSchema/oval-common-5}note'])
