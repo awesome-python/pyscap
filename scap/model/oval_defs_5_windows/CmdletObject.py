2bcf44f02f63321ee62b096d1a2a5d27f5091413
Casey Jaymes <cjaymes@gmail.com>
2016-07-24 12:11:20 -0400

    added cmdlet *

---
+++ scap/model/oval_defs_5_windows/CmdletObject.py
@@ -0,0 +1,36 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+from scap.model.oval_defs_5.Object import Object
+import logging
+
+logger = logging.getLogger(__name__)
+
+class CmdletObject(Object):
+    def __init__(self):
+        super(CmdletObject, self).__init__(
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}cmdlet_object')
+
+        self.ignore_sub_elements.extend([
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}module_name',
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}module_id',
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}module_version',
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}verb',
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}noun',
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}parameters',
+            '{http://oval.mitre.org/XMLSchema/oval-definitions-5#windows}select',
+        ])
