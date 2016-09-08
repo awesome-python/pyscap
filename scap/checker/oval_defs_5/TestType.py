b4cc75981fb99a22bec514d285daa4cf875a1341
Casey Jaymes <cjaymes@gmail.com>
2016-09-08 16:11:54 -0400

    fixed import order

--- scap/checker/oval_defs_5/TestType.py
+++ scap/checker/oval_defs_5/TestType.py
@@ -15,8 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
 
-from scap.Checker import Checker
 import logging
+
+from scap.Checker import Checker
 
 logger = logging.getLogger(__name__)
 class TestType(Checker):
