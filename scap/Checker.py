dc4b134a751feaf9a823f87cf3ab8f9c4a2ae569
Casey Jaymes <cjaymes@gmail.com>
2016-09-08 15:27:02 -0400

    added some debug info

--- scap/Checker.py
+++ scap/Checker.py
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
 
+import importlib
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 class Checker(object):
@@ -24,14 +26,12 @@ class Checker(object):
         model_namespace = content.__class__.__module__.split('.')[2]
         collector_module = 'scap.checker.' + model_namespace + '.' + content.__class__.__name__
         # try to load the collector's module
-        import sys, importlib
         if collector_module not in sys.modules:
             logger.debug('Loading module ' + collector_module)
-            import importlib
             try:
                 mod = importlib.import_module(collector_module)
-            except:
-                logger.warning('Could not load module for ' + collector_module)
+            except Exception as e:
+                logger.warning('Could not load module for ' + collector_module + ': ' + str(e))
                 raise
         else:
             mod = sys.modules[collector_module]
@@ -54,16 +54,8 @@ class Checker(object):
         raise NotImplementedError(inspect.stack()[0][3] + '() has not been implemented in subclass: ' + self.__class__.__name__)
 
     def resolve_reference(self, ref):
-        if ref in self.ref_mapping:
-            logger.debug('Mapping reference ' + ref + ' to ' + self.ref_mapping[ref])
-            ref = self.ref_mapping[ref]
-
         if not self.parent:
             raise RuntimeError("Got to null parent without resolving reference")
 
         logger.debug('Reference ' + ref + ' not in ' + self.__class__.__name__ + ' continuing to parent ' + self.parent.__class__.__name__)
         return self.parent.resolve_reference(ref)
-
-    def set_ref_mapping(self, mapping):
-        logger.debug('Updating reference mapping with ' + str(mapping))
-        self.ref_mapping.update(mapping)
