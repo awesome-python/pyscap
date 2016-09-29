39bc63a076cb371a3cc4c674abea211eeaf6f579
Admin - Casey Jaymes <a-jaymesc@s01.smsuite.local>
2016-09-29 14:43:01 -0400

    added FactsReq/Resp

---
+++ agent/FactsResponseMessage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import pprint
+
+from Message import Message
+
+logger = logging.getLogger(__name__)
+class FactsResponseMessage(Message):
+    # Payload is generated in request message
+    def __init__(self, payload):
+        super().__init__(payload)
+
+    def __str__(self):
+        return self.__class__.__name__ + '[type=' + str(self._type) + \
+            ', payload=\n' + \
+            pprint.pformat(self._payload, compact=True) + '\n]'
