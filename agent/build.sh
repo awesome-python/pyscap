d6ec90c4ff3b885bd9696bb93b6af9e13810a734
Casey Jaymes <cjaymes@gmail.com>
2016-10-12 10:50:53 -0400

    added dependency information to build.sh

--- agent/build.sh
+++ agent/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+#python -m pip install --upgrade pip
+# this isn't a true requirement(s.txt) because pytest is only used for testing and pyinstaller is only used for packaging, not runtime
+#pip install pytest pyinstaller
 pyinstaller -d -y --noupx --hidden-import='message.PingMessage' --hidden-import='message.FactsRequestMessage' --hidden-import='message.RunProcessMessage' agent.py
