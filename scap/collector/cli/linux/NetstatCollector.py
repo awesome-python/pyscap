d94c08b1562c527dc6a6a946dffcb68429ffb8ed
Casey Jaymes <cjaymes@gmail.com>
2017-04-02 11:33:31 -0400

    added summary

--- scap/collector/cli/linux/NetstatCollector.py
+++ scap/collector/cli/linux/NetstatCollector.py
@@ -30,3 +30,6 @@ class NetstatCollector(Collector):
                 # almost impossible to accurately figure out what the port is being used
                 # for, we use tcp & udp instead
                 self.host.facts['network_services'].append({'ip_address': m.group(2), 'port': m.group(3), 'protocol': m.group(1)})
+
+        for netsvc in self.host.facts['network_services']:
+            logger.debug('Service: Address: ' + netsvc['ip_address'] + ' Port: ' + netsvc['port'] + ' Protocol: ' + netsvc['protocol'])
