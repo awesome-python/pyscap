0926708c651666d6f53b5dd0dd56839278156acc
Casey Jaymes <cjaymes@gmail.com>
2017-04-07 10:42:08 -0400

    moved report attachment back

--- scap/Reporter.py
+++ scap/Reporter.py
@@ -162,6 +162,7 @@ class Reporter(object):
                 s.protocol.value = svc['protocol']
 
             report = ReportType()
+            arc.reports.reports.append(report)
             report.id = 'report_' + uuid.uuid4().hex
 
             rel = RelationshipType()
@@ -187,9 +188,6 @@ class Reporter(object):
             rel.refs.append(ref)
 
             # TODO 'hasMetadata' relationship
-
-            # moved report append to bottom to make it easier to find in the output
-            arc.reports.reports.append(report)
 
         arc_et = ET.ElementTree(element=arc.to_xml())
         return arc_et
